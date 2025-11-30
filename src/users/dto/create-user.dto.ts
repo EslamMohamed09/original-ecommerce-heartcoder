3533093040f30f79e5227997afe059cdf3b80a50
EslamMohamed09 <eslammayo2022@gmail.com>
2025-11-30 23:45:11 +0200

    npm install class-validator class-transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { userTypes } from 'src/shared/schema/users';
+
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsIn([userTypes.ADMIN, userTypes.CUSTOMER])
+    type: string;
+    secretToken?: string;
+}
